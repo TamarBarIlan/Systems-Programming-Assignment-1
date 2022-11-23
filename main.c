9f33d387f9fe1554407d8d529baccb4a3d96c858
TamarBarIlan <tamarbarilan.4@gmail.com>
2022-11-23 12:02:51 +0200

    new line in the end

--- main.c
+++ main.c
@@ -1,49 +1,57 @@
 #include <stdio.h>
 #include "NumClass.h"
 
-
 /*The program will ask for 2 whole and positive numbers from the user.
 The program will print to the screen 4 lines that will contain:
 prime numbers, Armstrong number, strong number ang palindrome number between the 2 numbers.
 */
 
-int main() {
-int start, end;
-scanf("%d", &start);
-scanf("%d", &end);
+int main()
+{
+   int start, end;
+   scanf("%d", &start);
+   scanf("%d", &end);
 
-printf("The Armstrong numbers are:");
-for( int i = start; i<=end; i++){
-   if(isArmstrong(i) == 1){
-      printf(" %d", i);
+   printf("The Armstrong numbers are:");
+   for (int i = start; i <= end; i++)
+   {
+      if (isArmstrong(i) == 1)
+      {
+         printf(" %d", i);
+      }
    }
-}
-printf("\n");
+   printf("\n");
 
-printf("The Palindromes are:");
-for( int i = start; i<=end; i++){
-   if(isPalindrome(i) == 1){
-      printf(" %d", i);
+   printf("The Palindromes are:");
+   for (int i = start; i <= end; i++)
+   {
+      if (isPalindrome(i) == 1)
+      {
+         printf(" %d", i);
+      }
    }
-}
- printf("\n");
+   printf("\n");
 
-printf("The Prime numbers are:");
+   printf("The Prime numbers are:");
 
-for( int i = start; i<=end; i++){
-   if(isPrime(i) == 1){
-      printf(" %d", i);
+   for (int i = start; i <= end; i++)
+   {
+      if (isPrime(i) == 1)
+      {
+         printf(" %d", i);
+      }
    }
-}
-printf("\n");
+   printf("\n");
 
-printf("The Strong numbers are:");
-for( int i = start; i<=end; i++){
-   if(isStrong(i) == 1){
-      printf(" %d", i);
+   printf("The Strong numbers are:");
+   for (int i = start; i <= end; i++)
+   {
+      if (isStrong(i) == 1)
+      {
+         printf(" %d", i);
+      }
    }
-}
+   printf("\n");
 
- return 0;
-
+   return 0;
 }
